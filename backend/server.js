cb174c3a1d05219008904e35e555070b819a3a67
Evelyne Otieno <evelyne.akeyo015@gmail.com>
2025-07-14 21:31:17 +0300

    Message

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ let dbName = 'yolomy';
 
 // define a url to connect to the database
 const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
-mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 let db = mongoose.connection;
 
 // Check Connection
